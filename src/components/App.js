e5495b2d4f0585d3f2d6833bc63d7bbd4335d0d6
Peter Amir <Peteramir5522@gmail.com>
2022-07-16 17:40:58 +0200

    Feat. connect both previous commited components to their parent compoenet

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,64 @@
+import { useState, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "../css/App.css";
+import ListContacts from "./ListContacts";
+import * as ContactApi from "../utils/ContactsAPI";
+import CreateContact from "./CreateContact";
 
 const App = () => {
-  return <div>Hello World</div>;
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+
+  const removeContact = (id) => {
+    ContactApi.remove(id);
+
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  };
+
+  const createContact = (contact) => {
+    const create = async () => {
+      const res = await ContactApi.create({
+        name: contact.name,
+        handle: contact.handle,
+      });
+      setContacts(contacts.concat(res));
+    };
+
+    create();
+    navigate("/");
+  };
+
+  useEffect(() => {
+    const getContacts = async () => {
+      const res = await ContactApi.getAll();
+
+      setContacts(res);
+    };
+
+    getContacts();
+  }, []);
+
+  return (
+    <div>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <ListContacts contacts={contacts} onDeleteContact={removeContact} />
+          }
+        />
+        <Route
+          path="/create"
+          element={
+            <CreateContact
+              onCreateContact={(contact) => createContact(contact)}
+            />
+          }
+        />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
